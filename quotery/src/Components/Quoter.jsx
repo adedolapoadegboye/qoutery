e8bec1741dda453b976585e7043717456e23d640
Adedolapo Adegboye <adedolapo.adegboye@alumni.ucalgary.ca>
2023-11-19 22:20:51 -0700

    Project Completed

--- quotery/src/Components/Quoter.jsx
+++ quotery/src/Components/Quoter.jsx
@@ -1,46 +1,79 @@
 import React from "react";
 import { BiSolidQuoteSingleLeft } from "react-icons/bi";
 import { FaXTwitter } from "react-icons/fa6";
-
+import { useState } from "react";
+import { colors } from "../Data/Colors";
+import { quotes } from "../Data/Quotes";
 
 const Quoter = () => {
+
+  const randomOne = Math.floor(Math.random() * colors.length);
+  const randomTwo = Math.floor(Math.random() * quotes.length);
+
+  const defaultColor = colors[randomOne]["paletteName"];
+  const defaultQuote = quotes[randomTwo]["quote"];
+  const defaultAuthor = quotes[randomTwo]["author"];
+
+  const [nextColor, setColor] = useState(defaultColor);
+  const [nextQuote, setQuote] = useState(defaultQuote);
+  const [nextAuthor, setAuthor] = useState(defaultAuthor);
+
+
+  function handleClick() {
+    setColor(colors[randomOne]["paletteName"]);
+    setQuote(quotes[randomTwo]["quote"]);
+    setAuthor(quotes[randomTwo]["author"]);
+  }
+
+  console.log(nextColor, nextQuote, nextAuthor, randomOne, randomTwo)
+
   return (
-    <div className="bg-blue-400 flex items-center justify-center h-screen w-screen">
+    <div className="bg-{nextColor}-400 flex items-center justify-center h-screen w-screen">
       <div
         id="quote-box"
-        className="bg-white min-w-[200px] md:min-w-[600px] text-blue-400"
+        className="bg-[nextColor] min-w-[200px] md:min-w-[600px] text-[nextColor]-400"
       >
         <div
-          className="font-bold font-serif subpixel-antialiased text-xl md:text-4xl text-center py-4
+          className="font-serif subpixel-antialiased text-xl md:text-2xl text-center py-4
  "
         >
-        <BiSolidQuoteSingleLeft size={40} className="mx-2"/>
-          <h1 id="text">Text</h1>
+          <BiSolidQuoteSingleLeft size={40} className="mx-2 lg:mx-15" />
+          <h1 className="mx-4 lg:mx-20" id="text">{nextQuote}</h1>
         </div>
         <div
           className="text-bold font-serif subpixel-antialiased text-sm md:text-2xl text-right px-4 py-4
  "
         >
-          <h1 className="italic" id="author">
-            - by <span>Author</span>
+          <h1 className="italic mx-4 lg:mx-20" id="author">
+            - by <span>{nextAuthor}</span>
           </h1>
         </div>
         <div
           className="text-bold font-serif subpixel-antialiased flex justify-center text-center my-3.5
  "
         >
-          <button className="cursor-pointer bg-blue-400 hover:bg-blue-300 text-white px-4 py-3.5 mx-4 my-4 border rounded-xl text-l md:text-xl flex" href="#" id="tweet-quote">
-           {/*  <h1>Post on</h1> */}
-            <FaXTwitter size={20} className="my-1 mx-1"/>
+          <button
+            className="cursor-pointer bg-{nextColor}-400 hidden hover:bg-[nextColor]-300 text-black px-4 py-3.5 mx-4 my-4 border rounded-xl text-l md:text-xl flex"
+            href="#"
+            id="tweet-quote"
+          >
+            {/*  <h1>Post on</h1> */}
+            <a href="twitter.com/intent/tweet?text=Hello%20World">
+            <FaXTwitter size={20} className="my-1 mx-1" />
+            </a>
           </button>
-          <button id="new-quote" className="cursor-pointer hover:bg-blue-300 bg-blue-400 text-white px-4 py-3.5 mx-4 my-4 border rounded-xl text-sm md:text-xl ">Get new quote</button>
+          <button
+            id="new-quote"
+            className="cursor-pointer hover:bg-blue-300 bg-[nextColor]-400 text-black px-4 py-3.5 mx-4 my-4 border rounded-xl text-sm md:text-xl "
+            onClick={()=>handleClick()}>
+            Get new quote
+          </button>
         </div>
       </div>
-{/*       <div>
-        <h1 className="text-white font-sans text-center">by ade</h1>
+      {/*       <div>
+        <h1 className="text-black font-sans text-center">by ade</h1>
       </div> */}
     </div>
-
   );
 };
 
